c884caf7461aa3818dc9513859b9fcd69df5f6d5
张鹤鸣 <zhangheming@Hemings-MacBook-Pro.local>
2026-06-05 19:42:48 +0800

    About Page draft of Korean Studies

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://zhanghm0625.github.io',
-  base: '/korean_studies_website',
+  base: '/korean_studies_website/',
 });
